81a6dcb3ee54e90b784f797e47c0e3ec6b3e378a
vuk.damjanovic <vuk.damjanovic@3stepit.com>
2022-09-07 15:53:51 +0300

    UI optimisations

--- ytdown.py
+++ ytdown.py
@@ -1,7 +1,12 @@
-import ssl
-import certifi
+from tkinter import filedialog
+
 from pytube import YouTube
 from tkinter import *
+
+
+def open_window():
+    location = filedialog.askdirectory()
+    return location
 
 
 class MyWindow:
@@ -11,34 +16,31 @@ class MyWindow:
         btn2 = StringVar()
         # labels
         self.lbl1 = Label(win, text='URL')
-        self.lbl2 = Label(win, text='Download location')
+        self.lbl2 = Label(win, text='Select download location')
         self.lbl3 = Label(win, text="Audio")
         self.lbl4 = Label(win, text="Video")
         # fields
-        self.t1 = Entry()
-        self.t2 = Entry()
+        self.t1 = Entry(width=50)
+        # self.location = Button(win, text="Select", command=open_window)
         self.r1 = Radiobutton(win, variable=btn1)
         self.r2 = Radiobutton(win, variable=btn2)
 
         self.lbl1.place(x=0, y=20)
         self.t1.place(x=0, y=40)
 
-        self.lbl2.place(x=0, y=80)
-        self.t2.place(x=0, y=100)
+        self.lbl3.place(x=0, y=80)
+        self.r1.place(x=50, y=80)
 
-        self.lbl3.place(x=0, y=150)
-        self.r1.place(x=50, y=150)
-
-        self.lbl4.place(x=100, y=150)
-        self.r2.place(x=150, y=150)
+        self.lbl4.place(x=100, y=80)
+        self.r2.place(x=150, y=80)
         # buttons
-        self.b1 = Button(win, text='Download', command=self.download)
-        self.b1.place(x=50, y=200)
+        self.b1 = Button(win, text='Select location and Download', command=self.download)
+        self.b1.place(x=50, y=100)
 
     def download(self):
         # get the variables
         link = self.t1.get()
-        downloadLocation = self.t2.get()
+        downloadLocation = open_window()
         isAudio = self.r1
 
         # main logic
@@ -46,7 +48,7 @@ class MyWindow:
         if isAudio:
             print("Downloading--> " + yt.title + " in audio format")
             file = yt.streams.get_audio_only("mp4")
-            file.download(downloadLocation)
+            file.download()
         else:
             print("Downloading--> " + yt.title + " in video format")
             file = yt.streams.get_highest_resolution()  # video
@@ -57,5 +59,5 @@ class MyWindow:
 window = Tk()
 mywin = MyWindow(window)
 window.title('YTDownloader')
-window.geometry("400x300+10+10")
+window.geometry("300x300")
 window.mainloop()
