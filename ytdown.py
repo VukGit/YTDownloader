b86bb3c6e1fd66b462f6274cc589a1841c5d4bb2
vuk.damjanovic <vuk.damjanovic@3stepit.com>
2022-09-07 11:25:07 +0300

    V1.1 - add UI to the app

--- ytdown.py
+++ ytdown.py
@@ -1,48 +1,74 @@
+import ssl
+import certifi
 from pytube import YouTube
-from sys import argv
-
 from tkinter import *
+
+
 class MyWindow:
 
-    link = ''
-
     def __init__(self, win):
-        self.lbl1=Label(win, text='URL')
-        self.lbl2=Label(win, text='Download location')
-        self.t1=Entry(bd=3)
-        self.t2=Entry()
+        btn1 = StringVar()
+        btn2 = StringVar()
+        # labels
+        self.lbl1 = Label(win, text='URL')
+        self.lbl2 = Label(win, text='Download location')
+        self.lbl3 = Label(win, text="Audio")
+        self.lbl4 = Label(win, text="Video")
+        # fields
+        self.t1 = Entry()
+        self.t2 = Entry()
+        self.r1 = Radiobutton(win, variable=btn1)
+        self.r2 = Radiobutton(win, variable=btn2)
 
-        self.btn1 = Button(win, text='Download')
-        self.lbl1.place(x=0, y=50)
-        self.t1.place(x=30, y=50)
+        self.lbl1.place(x=0, y=20)
+        self.t1.place(x=0, y=40)
 
-        self.lbl2.place(x=0, y=100)
-        self.t2.place(x=120, y=100)
-        self.b1=Button(win, text='Download', command=self.download)
+        self.lbl2.place(x=0, y=80)
+        self.t2.place(x=0, y=100)
 
-        self.b1.place(x=100, y=150)
+        self.lbl3.place(x=0, y=150)
+        self.r1.place(x=50, y=150)
+
+        self.lbl4.place(x=100, y=150)
+        self.r2.place(x=150, y=150)
+        # buttons
+        self.b1 = Button(win, text='Download', command=self.download)
+        self.b1.place(x=50, y=200)
+
     def download(self):
-        link=self.t1.get()
-        num2=self.t2.get()
-        print(self.t1.get())
-        print(self.t2.get())
+        # get the variables
+        link = self.t1.get()
+        downloadLocation = self.t2.get()
+        isAudio = self.r1
+
+        # main logic
+        yt = YouTube(link)
+        if isAudio:
+            print("Downloading--> " + yt.title + " in audio format")
+            file = yt.streams.get_audio_only("mp4")
+            file.download(downloadLocation)
+        else:
+            print("Downloading--> " + yt.title + " in video format")
+            file = yt.streams.get_highest_resolution()  # video
+            file.download(downloadLocation)
+        print("Done!")
 
 
-# # link, defined in the terminal command (position 2)
+# link, defined in the terminal command (position 2)
 # link = argv[1]
-# # download location (position 3)
+# download location (position 3)
 # downloadLocation = argv[2]
-# # download location (position 4)
+# download location (position 4)
 # whatToDownload = argv[3]
-# # creates an object with the link property
+# creates an object with the link property
 # yt = YouTube(link)
-#
-# #do the magic
-# if (whatToDownload == 'a'):
+
+# do the magic
+# if whatToDownload == 'a':
 #     print("Downloading--> " + yt.title + " in audio format")
 #     file = yt.streams.get_audio_only("mp4")  # audio
 #     file.download(downloadLocation)
-# elif (whatToDownload == 'v'):
+# elif whatToDownload == 'v':
 #     print("Downloading--> " + yt.title + " in video format")
 #     file = yt.streams.get_highest_resolution()  # video
 #     file.download(downloadLocation)
@@ -50,9 +76,8 @@ class MyWindow:
 #     print("Not a valid file type.")
 # print("Done!")
 
-window=Tk()
-mywin=MyWindow(window)
+window = Tk()
+mywin = MyWindow(window)
 window.title('YTDownloader')
 window.geometry("400x300+10+10")
 window.mainloop()
-
